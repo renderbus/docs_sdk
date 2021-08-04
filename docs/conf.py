3efddf3a5279897944341b9b150b772dc39c1e35
zhuangweiyang <zhuangweiyang@rayvision.com>
2021-08-04 14:42:43 +0800

    feat: Build failure repair

--- docs/conf.py
+++ docs/conf.py
@@ -55,8 +55,7 @@ extensions = [
     'sphinx.ext.ifconfig',
     # 'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
-    'sphinx_markdown_tables',
-    'recommonmark'
+    'sphinx_markdown_tables'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
